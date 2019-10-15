73972d79eb3c0db0dda6697a48e8cb5df3afe113
blackPavlin <boikiv.bogdan@gmail.com>
2019-10-15 04:07:35 +0400

    Implement user registration and authentication

--- README.md
+++ README.md
@@ -2,11 +2,21 @@
 
 
 ### Database URL
+
+Production
 ```
 postgres://postgres:123@localhost:5432/restapi_dev?sslmode=disable
+```
+
+Test
+```
+postgres://postgres:123@localhost:5432/restapi_test?sslmode=disable
 ```
 
 ### Migrations
 ```
 migrate -path migrations -database "postgres://postgres:123@localhost:5432/restapi_dev?sslmode=disable" up
+```
+```
+migrate -path migrations -database "postgres://postgres:123@localhost:5432/restapi_dev?sslmode=disable" down
 ```
--- internal/app/store/teststore/userrepository_test.go
+++ internal/app/store/teststore/userrepository_test.go
@@ -1,37 +1,32 @@
-package store_test
+package teststore_test
 
 import (
 	"testing"
 
 	"blackPavlin/http-rest-api.git/internal/app/model"
 	"blackPavlin/http-rest-api.git/internal/app/store"
+	"blackPavlin/http-rest-api.git/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
-	defer teardown("users")
+	s := teststore.New()
+	u := model.TestUser(t)
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@expample.org",
-	})
-	assert.NoError(t, err)
+	assert.NoError(t, s.User().Create(u))
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
-	defer teardown("users")
-
+	s := teststore.New()
 	email := "user@expample.org"
 	_, err := s.User().FindByEmail(email)
-	assert.Error(t, err)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
 
-	s.User().Create(&model.User{
-		Email: "user@expample.org",
-	})
-
-	u, err := s.User().FindByEmail(email)
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
